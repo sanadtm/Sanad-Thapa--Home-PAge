12a9872efe1c52317963612cf3eed96d6bcf8705
Sanad Thapa <sanadtm@gmail.com>
2021-08-24 13:31:08 -0700

    Added Java Script to the Form

--- js/index.js
+++ js/index.js
@@ -1,25 +1,42 @@
-// var img = document.createElement("img");
-// img.src = "/photo/sanad.jpg";
-// img.style.borderRadius = "50%";
-// img.style.display = "block";
-// img.style.marginLeft = "auto";
-// img.style.marginRight = "auto";
-// img.style.height = "200px";
-// document.body.appendChild(img);
-
-// var para = document.createElement("div");
-// para.innerHTML =
-// 	"<b>Hi!! My name is Sanad Thapa!! </b> and I am originally from Nepal moved to the US in 2014 with my family. Something interesting about being has to be that I was in the junior national team for Table Tennis in Nepal   back in 2011 and 2012 and I have been investing a lot in    stocks and crypto which has been a roller coaster of a ride. In my free time, I like to work out, play video games, sit by the campfire and hang out with friends and family and go to the table tennis clubs to work out. ";
-// para.style.padding = "40px";
-// para.style.width = "600px";
-// para.style.display = "block";
-// para.style.marginLeft = "auto";
-// para.style.marginRight = "auto";
-// document.body.append(para).appendChild(projects);
-
 function changeinto() {
 	document.getElementById("b_photo").src = "photo/sanad6.jpg";
 }
 function changeback() {
 	document.getElementById("b_photo").src = "photo/sanadpcn.jpg";
+}
+let form1 = document.querySelector("form");
+
+form1.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+	event.preventDefault();
+	let name = document.querySelector("#name");
+	let email = document.querySelector("#email");
+	let text = document.querySelector("#message");
+	if (
+		// Empty Form Check
+		name.value == "" &&
+		email.value == "" &&
+		text.value == ""
+	) {
+		console.warn("You Must enter Some data to Submit this form");
+	} else {
+		// Not Empty Form
+		console.log("======Form Submition=====");
+		if (name.value == "") {
+			console.log("Name  : No Submission ");
+		} else {
+			console.log("Name  : " + name.value);
+		}
+		if (email.value == "") {
+			console.log("Email     : No Submission ");
+		} else {
+			console.log("Email     : " + email.value);
+		}
+		if (text.value == "") {
+			console.log("Your Msg  : No Submission ");
+		} else {
+			console.log("Your Msg  :" + text.value);
+		}
+	}
 }
